48ec184933b99fb1cd5d07c59f5155f639780d82
DiemUyen <diemuyen0408@gmail.com>
2023-02-04 17:14:32 +0700

    Create and Cancel session schedule

---
+++ lib/src/features/connect_teacher/data/datasources/schedule_datasource/remote_data/model/session_schedule_model.dart
@@ -0,0 +1,41 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+import '../../../../../../topic_detail/data/datasources/topics_datasource/remote_data/model/topic_detail_model.dart';
+import '../../../../../domain/entities/session_schedule_entity.dart';
+
+part 'session_schedule_model.g.dart';
+
+@JsonSerializable()
+class SessionScheduleModel extends SessionScheduleEntity {
+  SessionScheduleModel({
+      this.id,
+      this.studentId,
+      this.topic,
+      this.teacherId,
+      this.startAt,
+      this.pickedUpAt,
+      this.status,
+      this.createdAt})
+      : super(
+          sessionId: id,
+          sessionStudentId: studentId,
+          sessionTopicId: topic?.id,
+          sessionTeacherId: teacherId,
+          sessionStatus: SessionScheduleStatus.values
+              .firstWhere((element) => element.name == status),
+        );
+
+  final String? id;
+  final String? studentId;
+  final TopicDetailModel? topic;
+  final String? teacherId;
+  final String? startAt;
+  final String? pickedUpAt;
+  final String? status;
+  final String? createdAt;
+
+  factory SessionScheduleModel.fromJson(Map<String, dynamic> json) => _$SessionScheduleModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$SessionScheduleModelToJson(this);
+}
--- lib/src/features/connect_teacher/presentation/blocs/connect_teacher_event.dart
+++ lib/src/features/connect_teacher/presentation/blocs/connect_teacher_event.dart
@@ -1,1 +1,31 @@
 part of 'connect_teacher_bloc.dart';
+
+abstract class ConnectTeacherEvent {
+  const ConnectTeacherEvent();
+}
+
+class ConnectTeacherInit extends ConnectTeacherEvent {
+  const ConnectTeacherInit(this.topicId);
+
+  final String topicId;
+}
+
+class ConnectTeacherCancelButtonPressed extends ConnectTeacherEvent {
+  const ConnectTeacherCancelButtonPressed(this.sessionId);
+
+  final String sessionId;
+}
+
+class ConnectTeacherConnectRoom extends ConnectTeacherEvent {
+  const ConnectTeacherConnectRoom(this.sessionId);
+
+  final String sessionId;
+}
+
+class ConnectTeacherTryConnect extends ConnectTeacherEvent {
+
+}
+
+class ConnectTeacherFindOtherTeacher extends ConnectTeacherEvent {
+
+}
