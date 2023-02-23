42a0a347badc0135d4c63961ae44f20431d765f9
Huỳnh Tấn Thọ <htho379@gmail.com>
2023-02-23 15:16:00 +0700

    Added 'Dongle' font and updated text sizes

--- lib/base/define/size.dart
+++ lib/base/define/size.dart
@@ -1,49 +1,48 @@
 import 'dart:ui';
 
 // Text size
-const displayLargeSize = 57.0;
-const displayMediumSize = 45.0;
-const displaySmallSize = 36.0;
+const displayLargeSize = 59.0;
+const displayMediumSize = 47.0;
+const displaySmallSize = 38.0;
 
-const headlineLargeSize = 32.0;
-const headlineMediumSize = 28.0;
-const headlineSmallSize = 24.0;
+const headlineLargeSize = 40.0;
+const headlineMediumSize = 36.0;
+const headlineSmallSize = 34.0;
 
-const titleLargeSize = 22.0;
-const titleMediumSize = 16.0;
-const titleSmallSize = 14.0;
+const titleLargeSize = 30.0;
+const titleMediumSize = 24.0;
+const titleSmallSize = 22.0;
 
-const labelLargeSize = 14.0;
-const labelMediumSize = 12.0;
-const labelSmallSize = 11.0;
+const labelLargeSize = 22.0;
+const labelMediumSize = 20.0;
+const labelSmallSize = 19.0;
 
-const bodyLargeSize = 16.0;
-const bodyMediumSize = 14.0;
-const bodySmallSize = 12.0;
+const bodyLargeSize = 24.0;
+const bodyMediumSize = 22.0;
+const bodySmallSize = 20.0;
 
-const displayLargeWeight = FontWeight.w400;
-const displayMediumWeight = FontWeight.w400;
-const displaySmallWeight = FontWeight.w400;
+const displayLargeWeight = FontWeight.w300;
+const displayMediumWeight = FontWeight.w300;
+const displaySmallWeight = FontWeight.w300;
 
-const headlineLargeWeight = FontWeight.w400;
-const headlineMediumWeight = FontWeight.w400;
-const headlineSmallWeight = FontWeight.w400;
+const headlineLargeWeight = FontWeight.w300;
+const headlineMediumWeight = FontWeight.w300;
+const headlineSmallWeight = FontWeight.w300;
 
-const titleLargeWeight = FontWeight.w400;
-const titleMediumWeight = FontWeight.w500;
-const titleSmallWeight = FontWeight.w500;
+const titleLargeWeight = FontWeight.w300;
+const titleMediumWeight = FontWeight.w400;
+const titleSmallWeight = FontWeight.w400;
 
-const labelLargeWeight = FontWeight.w500;
-const labelMediumWeight = FontWeight.w500;
-const labelSmallWeight = FontWeight.w500;
+const labelLargeWeight = FontWeight.w400;
+const labelMediumWeight = FontWeight.w400;
+const labelSmallWeight = FontWeight.w400;
 
-const bodyLargeWeight = FontWeight.w400;
-const bodyMediumWeight = FontWeight.w400;
-const bodySmallWeight = FontWeight.w400;
+const bodyLargeWeight = FontWeight.w300;
+const bodyMediumWeight = FontWeight.w300;
+const bodySmallWeight = FontWeight.w300;
 
 // Icon size
 const iconButtonSize = 18.0;
 
-
 // Button height
-const buttonHeight = 40.0;
+const buttonHeight = 48.0;
