5defafaf6f1dc626c56f73448b2803140b505e67
Huỳnh Tấn Thọ <htho379@gmail.com>
2023-02-25 22:44:01 +0700

    Replaced FilledButton with ElevatedButton

--- lib/base/widgets/buttons/filled_button.dart
+++ lib/base/widgets/buttons/filled_button.dart
@@ -24,45 +24,43 @@ class AppFilledButton extends StatefulWidget {
 class _AppFilledButtonState extends State<AppFilledButton> {
   @override
   Widget build(BuildContext context) => widget.icon == null
-      ? FilledButton(
+      ? ElevatedButton(
           onPressed: widget.onPressed,
-          style: FilledButton.styleFrom(
-            backgroundColor: AppColor.mainColor2,
+          style: ElevatedButton.styleFrom(
             elevation: 0.0,
+            backgroundColor: AppColor.mainColor2,
             shadowColor: AppColor.shadow,
             minimumSize: widget.minimumSize,
           ),
           child: Text(
             widget.text,
             style: const TextStyle(
-                fontFamily: 'Dongle',
-                fontSize: labelLargeSize,
-                fontWeight: labelLargeWeight,
-                color: AppColor.defaultFont
+              fontFamily: 'Dongle',
+              fontSize: labelLargeSize,
+              fontWeight: labelLargeWeight,
+              color: AppColor.defaultFont,
             ),
           ),
         )
       : ElevatedButton.icon(
           onPressed: widget.onPressed,
-          style: ElevatedButton.styleFrom(
-            backgroundColor: AppColor.white,
+          style: TextButton.styleFrom(
+            backgroundColor: AppColor.mainColor2,
             foregroundColor: AppColor.defaultFont,
+            elevation: 0.0,
+            shadowColor: AppColor.shadow,
+            minimumSize: widget.minimumSize,
             textStyle: const TextStyle(
               fontFamily: 'Dongle',
               fontSize: labelLargeSize,
               fontWeight: labelLargeWeight,
             ),
-            elevation: 0.0,
-            shadowColor: AppColor.shadow,
-            minimumSize: widget.minimumSize,
           ),
           icon: Icon(
             widget.icon!,
             size: iconButtonSize,
             color: AppColor.defaultFont,
           ),
-          label: Text(
-            widget.text,
-          ),
+          label: Text(widget.text),
         );
 }
