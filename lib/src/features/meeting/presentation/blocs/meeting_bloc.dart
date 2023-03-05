4aa01e1509065ad88f09d474b33f35a48a0bf3e4
DiemUyen <diemuyen0408@gmail.com>
2023-03-05 13:49:59 +0700

    Add Jitsi meet wrapper package

--- lib/src/features/meeting/presentation/blocs/meeting_bloc.dart
+++ lib/src/features/meeting/presentation/blocs/meeting_bloc.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:jitsi_meet_wrapper/jitsi_meet_wrapper.dart';
 
 part 'meeting_event.dart';
 part 'meeting_state.dart';
@@ -8,5 +12,29 @@ class MeetingBloc extends Bloc<MeetingEvent, MeetingState> {
     on<MeetingInitEvent>(_onInit);
   }
 
-  void _onInit(MeetingInitEvent event, emit) async {}
+  void _onInit(MeetingInitEvent event, emit) async {
+    var featureFlags = <FeatureFlag, bool>{
+      FeatureFlag.isWelcomePageEnabled: false,
+      FeatureFlag.isAddPeopleEnabled: false,
+    };
+
+    if (!kIsWeb) {
+      if (Platform.isAndroid) {
+        featureFlags[FeatureFlag.isCallIntegrationEnabled] = false;
+      } else if (Platform.isIOS) {
+        featureFlags[FeatureFlag.isPipEnabled] = false;
+      }
+    }
+
+    var options = JitsiMeetingOptions(
+      roomNameOrUrl: 'jsdfhnu394921ni92341njsd',
+      serverUrl: 'https://meet.jit.si/',
+      subject: 'Meeting room of A and B',
+      isAudioMuted: true,
+      isVideoMuted: true,
+      featureFlags: featureFlags,
+    );
+
+    await JitsiMeetWrapper.joinMeeting(options: options);
+  }
 }
