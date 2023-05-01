5db50a048b0e0b1439d14ac8da116ec47f90f6e4
Đức Ngô <ducngo@ucs-MacBook-Pro.local>
2023-05-01 18:20:16 +0700

    update ui vocabs card

--- lib/src/features/topic_detail/presentation/widgets/phrase_card.dart
+++ lib/src/features/topic_detail/presentation/widgets/phrase_card.dart
@@ -14,102 +14,137 @@ class PhraseCard extends StatelessWidget {
   final TopicPhraseEntity phraseEntity;
 
   @override
-  Widget build(BuildContext context) => Container(
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-        decoration: const BoxDecoration(
-          color: AppColor.white,
-          borderRadius: BorderRadius.all(Radius.circular(12.0)),
-          // boxShadow: [
-          //   BoxShadow(
-          //     color: AppColor.shadow.shade200,
-          //     blurRadius: 4.0,
-          //     spreadRadius: 0.0,
-          //     offset: const Offset(0, 2),
-          //   )
-          // ],
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              children: [
-                Expanded(
-                  child: Text(
-                    phraseEntity.topicPhrase ?? 'null phrase',
-                    style: const TextStyle(
-                      height: 0.9,
-                      fontSize: titleMediumSize,
-                      fontWeight: titleMediumWeight,
-                      color: AppColor.mainColor1,
-                    ),
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    if (phraseEntity.bookmark == null) {
-                      if (phraseEntity.topicPhraseId != null &&
-                          phraseEntity.topicPhraseId!.isNotEmpty) {
-                        context.read<TopicDetailBloc>().add(
-                            TopicDetailPhraseCreateBookmarkEvent(
-                                phraseId: phraseEntity.topicPhraseId!));
-                      }
-                    } else {
-                      if (phraseEntity.bookmark!.id != null &&
-                          phraseEntity.bookmark!.id!.isNotEmpty) {
-                        context.read<TopicDetailBloc>().add(
-                            TopicDetailPhraseRemoveBookmarkEvent(
-                                bookmarkId: phraseEntity.bookmark!.id!));
-                      }
-                    }
-                  },
-                  child: phraseEntity.bookmark != null
-                      ? const Icon(
-                          Icons.bookmark,
-                          color: AppColor.mainColor1,
-                          size: 24.0,
-                        )
-                      : const Icon(
-                          Icons.bookmark_border,
-                          color: AppColor.shadow,
-                          size: 24.0,
+  Widget build(BuildContext context) => Theme(
+        data: Theme.of(context).copyWith(dividerColor: Colors.transparent),
+        child: Container(
+          decoration: BoxDecoration(
+            color: AppColor.white,
+            borderRadius: BorderRadius.circular(12.0),
+            boxShadow: [
+              BoxShadow(
+                color: AppColor.shadow.shade200,
+                blurRadius: 4.0,
+                spreadRadius: 0.0,
+                offset: const Offset(1, 2),
+              )
+            ],
+          ),
+          clipBehavior: Clip.hardEdge,
+          child: ExpansionTile(
+            collapsedBackgroundColor: Colors.white,
+            backgroundColor: Colors.white,
+            expandedCrossAxisAlignment: CrossAxisAlignment.start,
+            childrenPadding: const EdgeInsets.only(
+              left: padding32,
+              bottom: padding12,
+            ),
+            title: Padding(
+              padding: const EdgeInsets.only(
+                top: padding4,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          phraseEntity.topicPhrase ?? 'null phrase',
+                          style: const TextStyle(
+                            height: 0.9,
+                            fontSize: titleMediumSize,
+                            fontWeight: titleMediumWeight,
+                            color: AppColor.mainColor1,
+                          ),
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
                         ),
-                )
-              ],
-            ),
-            Text(
-              phraseEntity.phrasePhonetic ?? 'no phonetic',
-              style: const TextStyle(
-                fontFamily: 'NotoSans',
-                fontSize: bodySmallSize * 0.6,
-                fontWeight: bodySmallWeight,
-                color: AppColor.defaultFont,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          if (phraseEntity.bookmark == null) {
+                            if (phraseEntity.topicPhraseId != null &&
+                                phraseEntity.topicPhraseId!.isNotEmpty) {
+                              context.read<TopicDetailBloc>().add(
+                                  TopicDetailPhraseCreateBookmarkEvent(
+                                      phraseId: phraseEntity.topicPhraseId!));
+                            }
+                          } else {
+                            if (phraseEntity.bookmark!.id != null &&
+                                phraseEntity.bookmark!.id!.isNotEmpty) {
+                              context.read<TopicDetailBloc>().add(
+                                  TopicDetailPhraseRemoveBookmarkEvent(
+                                      bookmarkId: phraseEntity.bookmark!.id!));
+                            }
+                          }
+                        },
+                        child: phraseEntity.bookmark != null
+                            ? const Icon(
+                                Icons.bookmark,
+                                color: AppColor.mainColor1,
+                                size: 24.0,
+                              )
+                            : const Icon(
+                                Icons.bookmark_border,
+                                color: AppColor.shadow,
+                                size: 24.0,
+                              ),
+                      )
+                    ],
+                  ),
+                  Text(
+                    phraseEntity.phrasePhonetic ?? 'no phonetic',
+                    style: const TextStyle(
+                      fontFamily: 'NotoSans',
+                      fontSize: bodySmallSize * 0.6,
+                      fontWeight: bodySmallWeight,
+                      color: AppColor.defaultFont,
+                    ),
+                  ),
+                  const SizedBox(height: spacing4),
+                  ...phraseEntity.phraseTranslations!.map(
+                    (e) => Text(
+                      e.phraseMeaning ?? 'null meaning',
+                      style: const TextStyle(
+                        height: 0.9,
+                        fontSize: titleMediumSize,
+                        fontWeight: titleMediumWeight,
+                        color: AppColor.defaultFont,
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-            const SizedBox(height: spacing4),
-            ...phraseEntity.phraseTranslations!.map(
-              (e) => Text(
-                e.phraseMeaning ?? 'null meaning',
-                style: const TextStyle(
-                  height: 0.9,
-                  fontSize: titleMediumSize,
-                  fontWeight: titleMediumWeight,
+            children: [
+              const SizedBox(height: spacing2),
+              ...phraseEntity.phraseExamples!.map(
+                (e) => Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "• ",
+                      style: TextStyle(
+                        height: 0.9,
+                        fontSize: bodyMediumSize,
+                        fontWeight: bodyMediumWeight,
+                      ),
+                    ),
+                    Expanded(
+                      child: Text(
+                        e.phraseExample ?? 'null example',
+                        style: const TextStyle(
+                          height: 0.9,
+                          fontSize: bodyMediumSize,
+                          fontWeight: bodyMediumWeight,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ),
-            const SizedBox(height: spacing2),
-            ...phraseEntity.phraseExamples!.map(
-              (e) => Text(
-                e.phraseExample ?? 'null example',
-                style: const TextStyle(
-                  height: 0.9,
-                  fontSize: bodyMediumSize,
-                  fontWeight: bodyMediumWeight,
-                ),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       );
 }
