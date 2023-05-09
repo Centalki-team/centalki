6ef44ff96f737a27b93ee76c80376dce616d4a57
Đức Ngô <ducngo@ucs-MacBook-Pro.local>
2023-05-09 19:05:44 +0700

    final CEN-521

--- lib/src/shared/extensions.dart
+++ lib/src/shared/extensions.dart
@@ -3,3 +3,13 @@ import 'package:dio/dio.dart';
 extension IsOk on Response {
   bool get isOk => (statusCode ?? 0 ~/ 100) == 2;
 }
+
+extension PasswordValidate on String {
+  /// regexp pattern r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[a-zA-Z\d\w\W]).{8,}$'
+
+  bool get validatePassword {
+    /// password rule: at least 1 uppercase, 1 numeric and 1 special character, min 8
+    var pattern = r'^(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[a-zA-Z\d\w\W]).{8,}$';
+    return RegExp(pattern).hasMatch(this);
+  }
+}
